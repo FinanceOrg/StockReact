5d15a25e062f4e7041bea8915b9e73a6c2d91ef6
Darkwolf520 <balazsunio@gmail.com>
2026-02-24 23:21:32 +0100

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")
+  const token = request.cookies.get("token")?.value
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url))
